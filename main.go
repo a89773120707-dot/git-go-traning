22a49840713a90d03e5d6c07012b8767c2420765
Anar <drop.bot@inbox.ru>
2026-02-11 13:05:14 +0300

    feat: add IsValidName function with basic validation

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 func Greet(name string) string {
@@ -13,9 +14,29 @@ func Greet(name string) string {
 
 	return "Hello " + name + "!"
 }
+
 func Square(n int) int {
 	return n * n
 }
+
+func IsValidName(name string) bool {
+	trimmed := strings.TrimSpace(name)
+	if trimmed == "" {
+		return false
+	}
+	if len(trimmed) < 2 || len(trimmed) > 50 {
+		return false
+	}
+
+	for _, r := range trimmed {
+		if !unicode.IsLetter(r) && r != ' ' && r != '-' && r != '\'' {
+			return false
+		}
+
+	}
+	return true
+}
+
 func main() {
 	fmt.Println(Greet(""))
 	fmt.Println(Greet("anar"))
@@ -23,5 +44,7 @@ func main() {
 
 	fmt.Println(Square(2))
 	fmt.Println(Square(4))
+
+	fmt.Println("Isvalidname('Alex'):", IsValidName("Alex"))
 
 }
