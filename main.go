9743e8462a11925577542937c90f01d18164f1be
a89773120707-dot <a89773120707@gmail.com>
2026-02-12 08:46:49 +0300

    feat: add fan in (#8)
    Co-authored-by: Anar <drop.bot@inbox.ru>

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"unicode"
 )
 
@@ -52,6 +53,40 @@ func UpperCheckAsci(name string) bool {
 
 }
 
+// fan in
+func gen1(ch chan<- int) {
+	for i := 1; i <= 4; i++ {
+		ch <- i*10 + 1
+	}
+}
+
+func gen2(ch chan<- int) {
+	for i := 1; i <= 4; i++ {
+		ch <- i*100 + 1
+	}
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	out := make(chan int)
+	wg := &sync.WaitGroup{}
+
+	for _, c := range cs {
+		wg.Add(1)
+		go func(c <-chan int) {
+			defer wg.Done()
+			for v := range c {
+				out <- v
+			}
+		}(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
 func main() {
 	fmt.Println(Greet(""))
 	fmt.Println(Greet("anar"))
@@ -66,5 +101,27 @@ func main() {
 
 	fmt.Println("UpperCheckAsci name anar - ", UpperCheckAsci("anar"))
 	fmt.Println("UpperCheckAsci name Anar - ", UpperCheckAsci("Anar"))
+
+	//fan in
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		gen1(ch1)
+		close(ch1)
+	}()
+
+	go func() {
+		gen2(ch2)
+		close(ch2)
+	}()
+
+	merged := merge(ch1, ch2)
+
+	for v := range merged {
+		fmt.Println("Получили из merged :", v)
+	}
+
+	fmt.Println("Все ...")
 
 }
